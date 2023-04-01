af534ca0a314576ae2b6cd8b08817bfdd2db2fe1
Roman Babik <kazantip2020rb@gmail.com>
2023-04-01 23:05:03 +0300

    hw8 finish

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from '@vimeo/player';
+
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const storageKeyTime = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function saveCurrentTime({ seconds }) {
+  localStorage.setItem(storageKeyTime, Math.round(seconds));
+}
+
+player
+  .setCurrentTime(10)
+  .then(function (seconds) {
+    seconds = localStorage.getItem(storageKeyTime)
+      ? player.setCurrentTime(localStorage.getItem(storageKeyTime))
+      : player.setCurrentTime(0);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        player.setCurrentTime(0);
+        break;
+
+      default:
+        player.setCurrentTime(0);
+        break;
+    }
+  });
+
 
